4d216eb10a002a899fb583bafb6a5309517e8037
jiadalfahmid <jiadalfahmid@gmail.com>
2024-10-14 23:58:23 +0600

    Starting Problem 11-18

---
+++ practice.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+   int length, width, perimeter;
+   length = 6;
+   width = 4;
+   perimeter = 2 * ( length + width);
+   
+   printf("Perimeter = %d\n", perimeter);
+}
